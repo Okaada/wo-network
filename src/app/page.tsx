844307748c41dede42e739beeafb82fe0fdfe885
Ravel Okada <ravelokada@gmail.com>
2024-09-10 19:20:07 -0300

    removing unused icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { AdjustmentsHorizontalIcon, ChatBubbleBottomCenterIcon, LightBulbIcon, PresentationChartBarIcon } from '@heroicons/react/24/outline';
+import { AdjustmentsHorizontalIcon, ChatBubbleBottomCenterIcon, LightBulbIcon } from '@heroicons/react/24/outline';
 import { ComputerDesktopIcon } from '@heroicons/react/24/outline';
 
 const CompanyPage = () => {
