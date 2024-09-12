254603245dbdeacfc7988ccb78f2e9a21022844f
Ravel Okada <ravelokada@gmail.com>
2024-09-11 21:58:42 -0300

    adjusting responsive stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 'use client';
-import React from 'react';
-import { AdjustmentsHorizontalIcon, ChatBubbleBottomCenterIcon, LightBulbIcon } from '@heroicons/react/24/outline';
-import { ComputerDesktopIcon } from '@heroicons/react/24/outline';
+import React, { useState } from 'react';
+import { AdjustmentsHorizontalIcon, ChatBubbleBottomCenterIcon, LightBulbIcon, Bars4Icon, XMarkIcon, ComputerDesktopIcon } from '@heroicons/react/24/outline';
 
 const CompanyPage = () => {
+  const [isOpen, setIsOpen] = useState(false); // Estado para controlar o menu hamburguer
+
   return (
     <div className="bg-gray-50 min-h-screen">
       {/* Adicionando scroll-padding-top no HTML */}
@@ -13,10 +14,23 @@ const CompanyPage = () => {
         }
       `}</style>
 
-      {/* Navbar fixa e centralizada */}
+      {/* Navbar fixa e hamburguer */}
       <nav className="fixed top-0 left-0 w-full bg-white bg-opacity-95 shadow-lg z-50">
-        <div className="container mx-auto flex justify-center items-center p-4">
-          <div className="flex space-x-8">
+        <div className="container mx-auto flex justify-between items-center p-4">
+          {/* Logo e Nome da Empresa à esquerda */}
+          <div className="flex items-center space-x-3">
+            <img src="/images/wo.png" alt="Logo W.O Network" className="h-auto w-auto" />
+          </div>
+
+          {/* Menu hamburguer para mobile */}
+          <div className="md:hidden">
+            <button onClick={() => setIsOpen(!isOpen)} className="text-yellow-500 focus:outline-none">
+              {isOpen ? <XMarkIcon className="h-8 w-8" /> : <Bars4Icon className="h-8 w-8" />}
+            </button>
+          </div>
+
+          {/* Links de Navegação para desktop */}
+          <div className={`hidden md:flex space-x-8`}>
             <a href="#apresentacao" className="text-yellow-500 font-bold hover:text-yellow-600 transition duration-300">Home</a>
             <a href="#clientes" className="text-yellow-500 font-bold hover:text-yellow-600 transition duration-300">Clientes</a>
             <a href="#trabalhos" className="text-yellow-500 font-bold hover:text-yellow-600 transition duration-300">Soluções</a>
@@ -24,6 +38,17 @@ const CompanyPage = () => {
             <a href="#contato" className="text-yellow-500 font-bold hover:text-yellow-600 transition duration-300">Contato</a>
           </div>
         </div>
+
+        {/* Menu hamburguer dropdown */}
+        {isOpen && (
+          <div className="md:hidden bg-white">
+            <a href="#apresentacao" className="block px-4 py-2 text-yellow-500 font-bold hover:text-yellow-600 transition duration-300">Home</a>
+            <a href="#clientes" className="block px-4 py-2 text-yellow-500 font-bold hover:text-yellow-600 transition duration-300">Clientes</a>
+            <a href="#trabalhos" className="block px-4 py-2 text-yellow-500 font-bold hover:text-yellow-600 transition duration-300">Soluções</a>
+            <a href="#sobre" className="block px-4 py-2 text-yellow-500 font-bold hover:text-yellow-600 transition duration-300">Sobre Nós</a>
+            <a href="#contato" className="block px-4 py-2 text-yellow-500 font-bold hover:text-yellow-600 transition duration-300">Contato</a>
+          </div>
+        )}
       </nav>
 
       {/* Hero Section */}
@@ -43,15 +68,15 @@ const CompanyPage = () => {
               Oferecemos o <span className="block">suporte que você</span> precisa para se destacar!
             </h2>
             <p className="mt-4 text-lg text-gray-700">
-              Impulsione sua empresa com soluções avançadas em infraestrutura e suporte especializado. Com expertise em redes, suporte técnico e otimização de processos, estamos preparados para criar abordagens personalizadas que garantem eficiência e segurança, permitindo que você foque no que realmente importa: o crescimento do seu negócio.
+              Impulsione sua empresa com soluções inovadoras de infraestrutura e suporte especializado. Com expertise em redes, suporte técnico e otimização de processos, oferecemos abordagens personalizadas que garantem máxima eficiência e segurança. Assim, você pode focar no que realmente importa: o crescimento sustentável e estratégico do seu negócio.
             </p>
             <a href="#contato" className="inline-block mt-6 bg-yellow-500 text-white py-3 px-6 rounded-lg hover:bg-yellow-600 transition duration-300">
               Fale Conosco
             </a>
           </section>
 
-          {/* Imagem ao lado direito */}
-          <div className="w-full md:w-1/2 pl-0 md:pl-16">
+          {/* Imagem ao lado direito, escondida no mobile */}
+          <div className="hidden md:block w-full md:w-1/2 pl-0 md:pl-16">
             <img
               src="/images/herosection.png"
               alt="Imagem de suporte"
@@ -62,8 +87,8 @@ const CompanyPage = () => {
         </div>
       </div>
 
-      {/* Seção de Clientes */}
-      <section id="clientes" className="py-12 bg-gray-50">
+      {/* Clientes Section */}
+      <section id="clientes" className="py-12 px-4 sm:px-8 bg-gray-50"> {/* Adicionado padding para telas menores */}
         <h2 className="text-2xl sm:text-4xl font-bold text-center mb-8" style={{ color: '#ffc929' }}>
           Nossos Clientes
         </h2>
@@ -87,15 +112,13 @@ const CompanyPage = () => {
           </div>
         </div>
 
-        {/* Texto opcional abaixo dos clientes */}
         <p className="text-center text-gray-700 mt-8">
           Estamos prontos para colaborar com você e sua empresa. Junte-se aos nossos clientes satisfeitos!
         </p>
       </section>
 
-
       {/* Trabalhos oferecidos */}
-      <section id="trabalhos" className="py-12 bg-gray-50">
+      <section id="trabalhos" className="py-12 px-4 sm:px-8 bg-white"> {/* Adicionado padding para telas menores */}
         <div className="container mx-auto text-center">
           <h2 className="text-3xl sm:text-5xl font-bold mb-8" style={{ color: '#ffc929' }}>
             Nossas Soluções
@@ -105,7 +128,7 @@ const CompanyPage = () => {
           </p>
 
           {/* Grid de Soluções */}
-          <ul className="grid grid-cols-1 md:grid-cols-3 lg:grid-cols-2 gap-8 lg:gap-10">
+          <ul className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-2 gap-8 lg:gap-10">
             {/* Suporte Técnico em destaque */}
             <li className="md:col-span-2 lg:col-span-2 bg-yellow-100 p-8 rounded-xl shadow-lg hover:shadow-xl transition-shadow duration-300 transform hover:scale-105 flex flex-col items-center text-center">
               <ChatBubbleBottomCenterIcon className="h-14 w-14 text-yellow-600 mb-4" />
@@ -116,7 +139,7 @@ const CompanyPage = () => {
             </li>
 
             {/* Soluções de Fibra Óptica */}
-            <li className="bg-white p-6 rounded-xl shadow-md hover:shadow-lg transition-shadow duration-300 transform hover:scale-105 flex flex-col items-center text-center">
+            <li className="md:col-span-2 lg:col-span-2 p-8 rounded-xl shadow-lg hover:shadow-xl transition-shadow duration-300 transform hover:scale-105 flex flex-col items-center text-center">
               <LightBulbIcon className="h-12 w-12 text-yellow-500 mb-4" />
               <h3 className="font-semibold text-xl text-gray-800">Soluções de Fibra Óptica</h3>
               <p className="mt-3 text-gray-600">
@@ -134,7 +157,7 @@ const CompanyPage = () => {
             </li>
 
             {/* Gestão de Processos */}
-            <li className=" md:col-span-2 lg:col-span-2 bg-white p-6 rounded-xl shadow-md hover:shadow-lg transition-shadow duration-300 transform hover:scale-105 flex flex-col items-center text-center">
+            <li className="bg-white p-6 rounded-xl shadow-md hover:shadow-lg transition-shadow duration-300 transform hover:scale-105 flex flex-col items-center text-center">
               <AdjustmentsHorizontalIcon className="h-12 w-12 text-yellow-500 mb-4" />
               <h3 className="font-semibold text-xl text-gray-800">Gestão de Processos</h3>
               <p className="mt-3 text-gray-600">
@@ -145,25 +168,36 @@ const CompanyPage = () => {
         </div>
       </section>
 
-
       {/* Sobre Nós */}
-      <section id="sobre" className="py-12 bg-gray-50">
-        <div className="container mx-auto text-center">
-          <h2 className="text-3xl sm:text-5xl font-bold mb-6" style={{ color: '#ffc929' }}>
-            Sobre Nós
-          </h2>
-          <p className="text-lg sm:text-xl max-w-3xl mx-auto text-gray-700 leading-relaxed">
-            Fundada em 3 de agosto de 2000, a W.O Network nasceu com o compromisso de simplificar a gestão de serviços de tecnologia da informação. Nossa missão é fornecer soluções tecnológicas eficientes, personalizadas e de alta qualidade, que atendam às necessidades específicas de cada cliente.
-          </p>
-          <p className="mt-6 text-lg sm:text-xl max-w-3xl mx-auto text-gray-700 leading-relaxed">
-            Com uma equipe altamente qualificada, atuamos em diversos projetos, desde Conectorização Óptica, Fusão e Certificação, até Cabeamento Estruturado, Organização de Racks, Manutenção em Computadores e Consultoria especializada em ambientes Windows e Linux. Estamos preparados para impulsionar sua infraestrutura tecnológica.
-          </p>
+      <section id="sobre" className="py-12 px-4 sm:px-8 bg-gray-50"> {/* Adicionado padding para telas menores */}
+        <div className="container mx-auto flex flex-col md:flex-row items-center justify-between">
+          {/* Texto à esquerda */}
+          <div className="w-full md:w-1/2 space-y-6">
+            <h2 className="text-3xl sm:text-5xl font-bold mb-6" style={{ color: '#ffc929' }}>
+              Sobre Nós
+            </h2>
+            <p className="text-lg sm:text-xl text-gray-700 leading-relaxed">
+              Fundada em 3 de agosto de 2000, a W.O Network nasceu com o compromisso de simplificar a gestão de serviços de tecnologia da informação. Nossa missão é fornecer soluções tecnológicas eficientes, personalizadas e de alta qualidade, que atendam às necessidades específicas de cada cliente.
+            </p>
+            <p className="text-lg sm:text-xl text-gray-700 leading-relaxed">
+              Com uma equipe altamente qualificada, atuamos em diversos projetos, desde Conectorização Óptica, Fusão e Certificação, até Cabeamento Estruturado, Organização de Racks, Manutenção em Computadores e Consultoria especializada em ambientes Windows e Linux. Estamos preparados para impulsionar sua infraestrutura tecnológica.
+            </p>
+          </div>
+
+          {/* Imagem à direita, escondida no mobile */}
+          <div className="hidden md:block w-full md:w-1/2 pl-0 md:pl-16">
+            <img
+              src="/images/about-bg.png"
+              alt="Imagem de sobre nós"
+              className="w-full h-auto object-cover"
+              style={{ maxHeight: '100vh' }}
+            />
+          </div>
         </div>
       </section>
 
-
       {/* Formulário de Contato */}
-      <section id="contato" className="py-8 sm:py-12 bg-white shadow-md rounded-lg">
+      <section id="contato" className="py-8 sm:py-12 px-4 bg-white shadow-md rounded-lg"> {/* Adicionado padding para telas menores */}
         <h2 className="text-2xl sm:text-4xl font-bold text-center" style={{ color: '#ffc929' }}>Contato para Orçamentos</h2>
         <form className="max-w-lg mx-auto mt-8 space-y-6">
           <div>
